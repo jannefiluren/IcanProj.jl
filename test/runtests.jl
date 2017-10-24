1f883323759404ad3162d8a2fa1214a2315cd280
jmgnve <jmgnve@nve.no>
2017-10-24 16:07:33 +0200

    clean up

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,30 @@ using IcanProj
 using Base.Test
 
 
+##########################################################################
 
+# Code for preparing data to vic and running simulations
 
-
-
-
-
-# Project scripts
+# 1. Prepare metadata for the experiment catchments
 
 #include("metadata_script.jl")
 
+# 2. Prepare vic input files using the metadata
+
+#include("vic_setup_data.jl")
+
+# 3. Run vic simulations
+
+#include("run_vic.jl")
+
+
+##########################################################################
+
+# Code for tranforming vic raw output files into useable format
+
+# 1. Average vic output and store in dataframes
+
+#include("process_vic_output.jl")
 
 
 
@@ -19,6 +33,10 @@ using Base.Test
 
 
 
+
+
+
+##########################################################################
 
 # Plotting scripts
 
