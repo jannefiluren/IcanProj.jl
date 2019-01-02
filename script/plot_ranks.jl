ce87942c746a562248968b859c29a48d7371dfe1
jmg <jmg@nve.no>
2019-01-02 12:55:29 +0100

    cleaning code

--- script/plot_ranks.jl
+++ script/plot_ranks.jl
@@ -1,65 +1,85 @@
-
+using IcanProj
 using DataFrames
 using JFSM2
 using PyPlot
 using PyCall
 using Statistics
-using IcanProj
-using CSV
 
 
-function load_results()
+function compute_error(path, res_coarse, variable)
 
-    res_all = Dict()
+    experiment = 1:32
 
-    for spaceres in ["5km", "10km", "25km", "50km"]
+    res = Vector{Dict}(undef, length(experiment))
+    
+    for iexp in experiment
 
-        file = joinpath(dirname(pathof(IcanProj)), "..", "data", "table_errors_$(spaceres).txt")
+        @show iexp
+        
+        # File names
+        
+        file_fine = get_filename(path, variable, "1km", iexp)
 
-        res_all[spaceres] = CSV.File(file, delim = ",") |> DataFrame
+        file_coarse = get_filename(path, variable, res_coarse, iexp)
+
+        # Load data
+
+        df_links = link_results(file_fine, file_coarse)
+
+        data_coarse, data_aggregated, ngrids = unify_results(file_fine, file_coarse, df_links, variable)
+
+        # Compute metrics per grid cell
+
+        err = data_coarse - data_aggregated
+
+        rmse = sqrt.(mean(err.^2 , dims = 1))
+
+        meanref = mean(data_aggregated, dims = 1)
+
+        meancmp = mean(data_coarse, dims = 1)
+
+        nrmse = rmse ./ meanref
+
+        bias = meancmp .- meanref
+
+        abs_bias = abs.(bias)
+
+        perc_bias = 100*(meancmp ./ meanref .- 1)
+
+        # Return as averages
+
+        weights = ngrids[:]/sum(ngrids[:])
+
+        res[iexp] = Dict("rmse" => sum(rmse[:] .* weights),
+                         "meanref" => sum(meanref[:] .* weights),
+                         "meancmp" => sum(meancmp[:] .* weights),
+                         "nrmse" => sum(nrmse[:] .* weights),
+                         "bias" => sum(bias[:] .* weights),
+                         "abs_bias" => sum(abs_bias[:] .* weights),
+                         "perc_bias" => sum(perc_bias[:] .* weights))
 
     end
 
-    return res_all
-    
+    return res
+
 end
 
 
-function ranking_of_options(res_all, variable, measure)
+function ranking_of_options(resall, variable, measure)
 
-    # Determine performance for each configuration
+    data_rmse = map(x -> x[measure], resall)
 
-    spaceres = "50km"
-
-    cfgs = 1:32
-
-    data = fill(0.0, length(cfgs))
-
-    df_res = res_all[spaceres]
-
-    for i in cfgs
-
-        tmp = df_res[Symbol("$(measure)_$(variable)_cfg$(cfgs[i])")]
-
-        tmp = tmp[map(x -> !isnan(x), tmp)]   
-
-        data[i] = median(tmp)
-
-    end
-
-    # Sort the configuration table after performance
-    
     df_cfg = cfg_table()
 
     df_cfg = df_cfg[[:exchng, :hydrol, :albedo, :condct, :densty]]
 
-    mat_cfg = convert(Array{Int64,2}, df_cfg)
+    mat_cfg = convert(Array{Int64, 2}, df_cfg)
 
     if measure == "nse"
         data = 1 .- data
     end
 
-    isorted = sortperm(data)
+    isorted = sortperm(data_rmse)
 
     mat_cfg = mat_cfg[isorted, :] 
 
@@ -72,9 +92,15 @@ function ranking_of_options(res_all, variable, measure)
 end
 
 
-# Load results
+# Error different scales - overall settings
 
-res_all = load_results()
+pathres = "/data04/jmg/fsm_simulations/netcdf/fsmres_forest"
+
+res_coarse = "50km"
+
+measure = "rmse"
+
+pathfig = joinpath(dirname(pathof(IcanProj)), "..", "plots", "error_scales")
 
 
 # Set up plot
@@ -83,9 +109,6 @@ py"""
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-
-xtext = $xtext
-ytext = $ytext
 
 fig, ax = plt.subplots(2, 2, figsize=(6,6)) 
 
@@ -96,28 +119,36 @@ fig.text(0.04, 0.5, "Ranking from lowest to highest error", va="center", rotatio
 
 # Snow water equivalent
 
-mat_cfg, xtext, ytext = ranking_of_options(res_all, "swe", "rmse")
+variable = "swe"
+
+resall = compute_error(pathres, res_coarse, variable)
+
+mat_cfg, xtext, ytext = ranking_of_options(resall, variable, measure)
 
 py"""
 data = $mat_cfg
 
-ax[0,0].pcolor(data, edgecolors='k', cmap=cmap, norm=norm, alpha = 0.5)
-ax[0,0].set_xticks(np.arange(0.5, len(xtext), 1)) #, xtext, rotation = 45)
+ax[0,0].pcolor(data, edgecolors='k', cmap=cmap, norm=norm, alpha = 1.0)
+ax[0,0].set_xticks(np.arange(0.5, len($xtext), 1)) #, xtext, rotation = 45)
 ax[0,0].set_xticklabels("")
 ax[0,0].set_yticks(np.arange(0.5, 32, 2)) #, ytext)
-ax[0,0].set_yticklabels(ytext)
+ax[0,0].set_yticklabels($ytext)
 ax[0,0].set_title("SWE")
 """
 
 # Net radiation
 
-mat_cfg, xtext, ytext = ranking_of_options(res_all, "rnet", "rmse")
+variable = "rnet"
+
+resall = compute_error(pathres, res_coarse, variable)
+
+mat_cfg, xtext, ytext = ranking_of_options(resall, variable, measure)
 
 py"""
 data = $mat_cfg
 
-ax[0,1].pcolor(data, edgecolors='k', cmap=cmap, norm=norm, alpha = 0.5)
-ax[0,1].set_xticks(np.arange(0.5, len(xtext), 1)) #, xtext, rotation = 45)
+ax[0,1].pcolor(data, edgecolors='k', cmap=cmap, norm=norm, alpha = 1.0)
+ax[0,1].set_xticks(np.arange(0.5, len($xtext), 1)) #, xtext, rotation = 45)
 ax[0,1].set_xticklabels("")
 ax[0,1].set_yticks(np.arange(0.5, 32, 2)) #, ytext)
 ax[0,1].set_yticklabels("")
@@ -126,29 +157,37 @@ ax[0,1].set_title("RNET")
 
 # Sensible heat fluxes
 
-mat_cfg, xtext, ytext = ranking_of_options(res_all, "hatmo", "rmse")
+variable = "hatmo"
+
+resall = compute_error(pathres, res_coarse, variable)
+
+mat_cfg, xtext, ytext = ranking_of_options(resall, variable, measure)
 
 py"""
 data = $mat_cfg
 
-ax[1,0].pcolor(data, edgecolors='k', cmap=cmap, norm=norm, alpha = 0.5)
-ax[1,0].set_xticks(np.arange(0.5, len(xtext), 1)) 
-ax[1,0].set_xticklabels(xtext, rotation = 45)
+ax[1,0].pcolor(data, edgecolors='k', cmap=cmap, norm=norm, alpha = 1.0)
+ax[1,0].set_xticks(np.arange(0.5, len($xtext), 1)) 
+ax[1,0].set_xticklabels($xtext, rotation = 45)
 ax[1,0].set_yticks(np.arange(0.5, 32, 2))
-ax[1,0].set_yticklabels(ytext)
+ax[1,0].set_yticklabels($ytext)
 ax[1,0].set_title("HATMO")
 """
 
 # Latent heat fluxes
 
-mat_cfg, xtext, ytext = ranking_of_options(res_all, "latmo", "rmse")
+variable = "latmo"
+
+resall = compute_error(pathres, res_coarse, variable)
+
+mat_cfg, xtext, ytext = ranking_of_options(resall, variable, measure)
 
 py"""
 data = $mat_cfg
 
-ax[1,1].pcolor(data, edgecolors='k', cmap=cmap, norm=norm, alpha = 0.5)
-ax[1,1].set_xticks(np.arange(0.5, len(xtext), 1)) 
-ax[1,1].set_xticklabels(xtext, rotation = 45)
+ax[1,1].pcolor(data, edgecolors='k', cmap=cmap, norm=norm, alpha = 1.0)
+ax[1,1].set_xticks(np.arange(0.5, len($xtext), 1)) 
+ax[1,1].set_xticklabels($xtext, rotation = 45)
 ax[1,1].set_yticks(np.arange(0.5, 32, 2))
 ax[1,1].set_yticklabels("")
 ax[1,1].set_title("LATMO")
@@ -164,149 +203,3 @@ plt.savefig($figname, dpi=200, bbox_inches='tight')
 plt.close()
 """
 
-
-
-
-
-
-
-#=
-
-function load_results()
-
-    res_all = Dict()
-
-    for spaceres in ["5km", "10km", "25km", "50km"]
-
-        file = joinpath(dirname(pathof(IcanProj)), "..", "data", "table_errors_$(spaceres).txt")
-
-        res_all[spaceres] = CSV.File(file, delim = ",") |> DataFrame
-
-    end
-
-    return res_all
-    
-end
-
-
-
-function error_matrix(res_all, variable, measure)
-
-    spaceres = ["5km", "10km", "25km", "50km"]
-
-    cfgs = 1:32
-
-    data = fill(0.0, length(cfgs), length(spaceres))
-
-    for j in eachindex(spaceres)
-
-        df_res = res_all[spaceres[j]]
-
-        for i in cfgs
-
-            tmp = df_res[Symbol("$(measure)_$(variable)_cfg$(cfgs[i])")]
-
-            tmp = tmp[map(x -> !isnan(x), tmp)]   
-
-            data[i,j] = median(tmp)
-
-        end
-        
-    end
-
-    return data, spaceres
-
-end
-
-
-
-
-function plot_rank(data, ylabrank, titlerank, filename)
-
-    df_cfg = cfg_table()
-
-    mat_cfg = convert(Array{Int64,2}, df_cfg)
-    
-    isorted = sortperm(data)
-
-    mat_cfg = mat_cfg[isorted, :] 
-
-    xtext = uppercase.(string.(names(df_cfg)))
-    ytext = collect(1:2:32) #round.(data[isorted], 3)
-    
-    ikeep = [1,3,4,5,6]
-    
-    xtext = xtext[ikeep]
-    ytext = ytext
-
-    mat_cfg = mat_cfg[:, ikeep]
-
-    py"""
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import matplotlib.colors as mcolors
-
-    xtext = $xtext
-    ytext = $ytext
-    data = $mat_cfg
-
-    cmap, norm = mcolors.from_levels_and_colors([-100, 0.5, 100], ['red', 'blue'])
-
-    plt.figure(figsize=(6, 6))
-    plt.pcolor(data, cmap=cmap, norm=norm, alpha = 0.5)
-    plt.xticks(np.arange(0.5, len(xtext), 1), xtext, rotation = 45)
-    plt.yticks(np.arange(0.5, 32, 2), ytext)
-    plt.colorbar()
-    plt.ylabel($ylabrank)
-    plt.title($titlerank)
-
-    plt.show()
-    plt.savefig($filename, dpi=200, bbox_inches='tight')
-    plt.close()
-    """
-    
-end
-
-
-
-res_all = load_results()
-
-figpath = joinpath(dirname(pathof(IcanProj)), "..", "plots", "rank")
-
-
-# Snow water equivalent
-
-filename = joinpath(figpath, "swe_rmse.png")
-
-res, spaceres = error_matrix(res_all, "swe", "rmse")
-
-plot_rank(res[:,end], "Ranking from lowest to highest error", "SWE", filename)
-
-
-# Latent heat exchange
-
-filename = joinpath(figpath, "latmo_rmse.png")
-
-res, spaceres = error_matrix(res_all, "latmo", "rmse")
-
-plot_rank(res[:,end], "Ranking from lowest to highest error", "LATMO", filename)
-
-
-# Sensible heat exchange
-
-filename = joinpath(figpath, "hatmo_rmse.png")
-
-res, spaceres = error_matrix(res_all, "hatmo", "rmse")
-
-plot_rank(res[:,end], "Ranking from lowest to highest error", "HATMO", filename)
-
-
-# Net radiation
-
-filename = joinpath(figpath, "rnet_rmse.png")
-
-res, spaceres = error_matrix(res_all, "rnet", "rmse")
-
-plot_rank(res[:,end], "Ranking from lowest to highest error", "RNET", filename)
-
-=#
